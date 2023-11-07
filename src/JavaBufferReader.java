352a1232d4e91cfbecb76d8d1636e98ae0c4ddc8
Whilmar Bitoco <whlmrbitoco@gmail.com>
2023-11-07 10:53:22 +0800

    fixed the bufferedreader file input to user input

--- src/JavaBufferReader.java
+++ src/JavaBufferReader.java
@@ -1,21 +1,29 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class JavaBufferReader {
 
 	public static void main(String[] args) {
 		
-		try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
+		try (BufferedReader input = new BufferedReader(new InputStreamReader(System.in));) {
 			
-			while (reader.readLine() != null) {
-				System.out.println(reader.readLine());
-			}
+			System.out.print("What is your name? ");
+			String name = input.readLine();
+			
+			System.out.println("hello, " + name);
+			
+			input.close();
 			
 		} catch (IOException e) {
-			System.out.println(e);
+			System.err.println(e);
 		}
+		
+		
+		
+		
+		
 		
 	}
 }
